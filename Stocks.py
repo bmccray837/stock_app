de691e78822f0616776ff73112817514b3ce4da2
bmccray <107178901+bmccray837@users.noreply.github.com>
2022-06-16 07:05:47 -0400

    Added earnings yield and ROE rankings

--- Stocks.py
+++ Stocks.py
@@ -1,7 +1,10 @@
 import yfinance as yf
 
 stock_picks = []
-stock_dict = {}
+stock_ranking = {}
+ey_rank = {}
+roe_rank = {}
+
 while True:
     try:
         num_stocks = int(input("Enter number of stocks you would like to research: "))
@@ -23,14 +26,17 @@ while i < num_stocks:
 for stock in stock_picks:
     try:
         stock_data = yf.Ticker(stock).info
-        stock_metrics = []
         stock_price = stock_data['regularMarketPrice']
         earnings_yield = stock_data['operatingMargins'] * stock_data['totalRevenue'] / stock_data['enterpriseValue'] * 100
-        stock_metrics.extend([stock_price, earnings_yield])
-        stock_dict[stock] = stock_metrics
+        roe = stock_data['returnOnEquity']
+        ey_rank[stock] = round(earnings_yield, 2)
+        roe_rank[stock] = round(roe * 100, 2)
         print("{stock} is currently trading at ${price} and has an earnings yield of {r}%.".format(stock = stock, price = stock_price, r = round(earnings_yield, 2)))
     except KeyError:
         continue
+
+for stock, ey in ey_rank.items():
+    
 
 # for stock in russell3000:
 #     stock_data = yf.Ticker(stock).info
